89f2e466fa7ace87a2e3143245ea02e92e3a814c
joaoArruda <joaoarrudarosa@hotmail.com>
2019-04-11 22:16:49 -0300

    criacao da tabela curso

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_004831) do
+ActiveRecord::Schema.define(version: 2019_04_12_005634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2019_04_12_004831) do
     t.string "mae"
     t.string "endereco"
     t.integer "documento"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cursos", force: :cascade do |t|
+    t.string "nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
