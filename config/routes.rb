de70f9ba91be94edea687fdef0f9caba49385434
joaoArruda <joaoarrudarosa@hotmail.com>
2019-04-11 21:30:51 -0300

    bug do server resolvido

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   resources :usuarios
-=======
+
   resources :disciplinas
->>>>>>> 46b7bf2f11b37551d2b01a5f4ff1978bd00a37be
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
