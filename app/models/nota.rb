2bb27c2b98304a5712bc53037c52574400f33cc1
eriqueg <guizanoni1234@gmail.com>
2019-05-03 22:29:23 -0300

    arrumando chicebox das notas do trabalho

--- app/models/nota.rb
+++ app/models/nota.rb
@@ -3,7 +3,5 @@ class Nota < ApplicationRecord
   belongs_to :professor
   belongs_to :disciplina
 
-  validates :nota_minima_float, numericality: { greater_than_or_equal_to: 0, 
-                                           less_than_or_equal_to: 10,
-                message: "A 'nota' deve ser entre 0 e 10" }
+ 
 end
--- app/models/presenca.rb
+++ app/models/presenca.rb
@@ -1,3 +1,5 @@
 class Presenca < ApplicationRecord
   belongs_to :curso
+  belongs_to :aluno
+  belongs_to :professor
 end
